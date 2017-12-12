f650deccf8a71a438ce3cf68660c398b6256d368
Andrew <ajestkov@gmail.com>
2017-12-12 16:05:40 +0300

    start

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import './index.css';
+import { Provider } from 'react-redux';
+import {Router, Route, Switch, BrowserHistory} from 'react-router';
 import App from './App';
-
+3
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+        <Provider store={ store }>
+
+        </Provider>
     </AppContainer>,
     document.getElementById('root'),
   )
